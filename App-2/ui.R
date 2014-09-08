8b5a364792979baa0b1afb0f5db59a4532beb49c
Shannon Buckley <buckley.shannont@gmail.com>
2014-09-08 15:16:51 -0700

    html tag examples added

--- App-2/ui.R
+++ App-2/ui.R
@@ -1,20 +1,21 @@
 library(shiny)
 
 shinyUI(fluidPage(
-    
-    # Application title
-    titlePanel("Exploring UI.R and HTML5 stuff"),
-    
-    # Sidebar with a slider input for the number of bins
-    sidebarLayout(#position="right",
-        sidebarPanel( "sidebar panel title"),
+    titlePanel("HTML tags in Shiny"),
+    sidebarLayout(
+        sidebarPanel(),
         mainPanel(
-                  h1("First Title Level"),
-                  h2("Second Title Level"),
-                  h3("Third Title Level", align="center"),
-                  h4("Fourth Title Level"),
-                  h5("Fifth Title Level"),
-                  h6("Sixth Title Level")
+            p("p creates a paragraph of text. Note: this paragraph is followed by br(), which makes a blank line."),
+            p("A new p() command starts a new paragraph. Supply a style attribute to change the format of the entire paragraph", style = "font-family: 'times'; font-si16pt"),
+            strong("strong() makes bold text."),
+            em("em() creates italicized (i.e, emphasized) text."),
+            br(),
+            code("code displays your text similar to computer code"),
+            div("div creates segments of text with a similar style. This division of text is all blue because I passed the argument 'style = color:blue' to div", style = "color:blue"),
+            br(),
+            p("span does the same thing as div, but it works with",
+              span("groups of words", style = "color:blue"),
+              "that appear inside a paragraph.")
         )
     )
 ))
