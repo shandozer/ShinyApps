bc38c90a1fb796bca2f0982b411395883ac028ba
shannon buckley <shannon.buckley@ucsf.edu>
2014-10-04 17:02:20 -0700

    censusVis: added reactive text output for input vars (slider and drop down). Also adding files that will be used in the next round of improvements.

--- censusVis/server.R
+++ censusVis/server.R
@@ -1,7 +1,17 @@
 library(shiny)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-    
+# server.R
 
-})
+shinyServer(
+    function(input, output) {
+        
+        output$text1 <- renderText({ 
+            paste("You have selected", input$var)
+        })
+        
+        output$text2 <- renderText({
+            paste("You have selected a range from ", input$range[1], " to ", input$range[2])
+        })
+        
+    }
+)
--- censusVis/ui.R
+++ censusVis/ui.R
@@ -2,18 +2,27 @@
 
 shinyUI(fluidPage(
     titlePanel("censusVis"),
+    
     sidebarLayout(
-    sidebarPanel(
-        helpText("Create demographic maps with information from 2010 US Census."),
-
-        selectInput("var", label = "Choose a variable to display", 
-                   choices = list("Percent White" = 1, "Percent Black" = 2,
-                                  "Percent Hispanic" = 3, "Percent Asian" = 4), selected = 1),
-
-        sliderInput("range", "Range of interest:",
-                   min = 0, max = 100, value = c(0, 100))
-        
+        sidebarPanel(
+            helpText("Create demographic maps with 
+        information from the 2010 US Census."),
+            
+            selectInput("var", 
+                        label = "Choose a variable to display",
+                        choices = c("Percent White", "Percent Black",
+                                    "Percent Hispanic", "Percent Asian"),
+                        selected = "Percent White"),
+            
+            sliderInput("range", 
+                        label = "Range of interest:",
+                        min = 0, max = 100, value = c(0, 100))
         ),
-    mainPanel()
+        
+        mainPanel(
+            textOutput("text1"), 
+            br(),
+            textOutput("text2")
+        )
     )
-    ))
+))
