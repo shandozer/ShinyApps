addfa5999292156f8054a22f385cf800bfedcd94
shannon buckley <shannon.buckley@ucsf.edu>
2014-10-07 19:04:12 -0700

    ok back to normal for app-1

---
+++ App-1/ui.R
@@ -0,0 +1,27 @@
+#app-1
+library(shiny)
+
+# UI uses a slider to control number of bins displayed in Histogram
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+    
+    # Application title
+    titlePanel("Welcome to my First Shiny App!"),
+    
+    # Sidebar with a slider input for the number of bins
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 5,
+                        max = 50,
+                        value = 30)
+        ),
+        
+        # Show a plot of the generated distribution
+        mainPanel(
+            plotOutput("distPlot")
+        )
+    )
+))
