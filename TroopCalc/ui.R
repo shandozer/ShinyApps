008fe2675aabe392f096b1dfeba998de867f954b
shannon buckley <shannon.buckley@ucsf.edu>
2014-10-17 19:04:07 -0700

    TroopCalc v1.1: Bootstrap added, Layout adjusted, needs more layout tweaks and responsiveness, still!

--- TroopCalc/ui.R
+++ TroopCalc/ui.R
@@ -1,21 +1,21 @@
 library(shiny)
 # ui.R
 # 
-VERSION = '1.0'
+VERSION = '1.1'
 
-shinyUI(fluidPage(
+shinyUI(fluidPage(title="TroopCalculator",
 #     titlePanel("TroopCalc"),
 #     includeCSS("styles.css"),
     
     tags$head(
-        tags$link(rel="stylesheet", type = "text/css", href="styles.css")),
-    headerPanel("TroopCalc"),
+        tags$link(rel="stylesheet", type = "text/css", href="bootstrap.css")),
+    headerPanel(h1(paste("Welcome to TroopCalc", VERSION), align = "center")),
     #titlePanel("Welcome to TroopCalc"),
         
     sidebarLayout(
     sidebarPanel(
         h3(helpText("Enter your information...")),
-        fluidRow(
+        fluidRow(theme = "bootstrap.css",
             column(3,
                    radioButtons("th", label = h5("Town Hall Level"),
                                 choices = list("5" = 1, "6" = 2,
@@ -46,54 +46,69 @@ shinyUI(fluidPage(
 
     mainPanel(
         fluidRow(
-
             column(3, 
-                   sliderInput("barbs", label = h5("Add Barbarians"),
-                               min = 0, max = 100, value = 0),  
-                   br(),
-                   sliderInput("arch", label = h5("Add Archers"),
-                               min = 0, max = 100, value = 0),  
-                   br(),
-                   sliderInput("gobs", label = h5("Add Goblins"),
-                               min = 0, max = 100, value = 0),  
-                   br(),
-                   sliderInput("giants", label = h5("Add Giants"),
-                               min = 0, max = 100, value = 0),  
-                   br(),  
-                   sliderInput("wb", label = h5("Add Wall Breakers"),
-                               min = 0, max = 100, value = 0)
+                   checkboxGroupInput("army_group", 
+                                      label = h4("Troops Available", align = "center"), 
+                                      choices = list("Barbarians" = 1, 
+                                                     "Archers" = 2, 
+                                                     "Goblins" = 3,
+                                                     "Giants" = 4,
+                                                     "Wall Breakers" = 5,
+                                                     "Wizards" = 6,
+                                                     "Healers" = 7,
+                                                     "Dragons" = 8,
+                                                     "Pekka" = 9),
+                                      selected = 1)),
+            column(4,
+                   radioButtons("barb_lvl", label = h4("Barbarians", align = "center", color = "black"),
+                                choices = list("1" = 1, "2" = 2,
+                                               "3" = 3, "4" = 4,
+                                               "5" = 5, "6" = 6),
+                                selected = 1, inline=T),
+                   
+                   radioButtons("arch_lvl", label = h4("Archers", align = "center"),
+                                choices = list("1" = 1, "2" = 2,
+                                               "3" = 3, "4" = 4,
+                                               "5" = 5, "6" = 6),
+                                selected = 1, inline=T),
+                   
+                   radioButtons("gob_lvl", label = h4("Goblins", align = "center"),
+                                choices = list("1" = 1, "2" = 2,
+                                               "3" = 3, "4" = 4,
+                                               "5" = 5, "6" = 6),
+                                selected = 1, inline=T)
+                   )
+        ),
+        br(),
+        hr(),
+        h4("Compose an army", align = "center"),
+        fluidRow(
+            
+            column(3, 
+                   sliderInput("num_wb", label = h5("Wall Breakers"),
+                               min = 0, max = 15, value = 4, step = 2)
                    ),
             
-            column(3,
-                   numericInput("barblvl", label = h5("Level"), 
-                                value = 1,
-                                min = 1,
-                                max = 6),  
-                   br(),
-                   br(),
-                   numericInput("archlvl", label = h5("Level"), 
-                                value = 1,
-                                min = 1,
-                                max = 6), 
-                   br(),
-                   numericInput("goblvl", label = h5("Level"), 
-                                value = 1,
-                                min = 1,
-                                max = 6),  
-                   br(),
-                   br(),
-                   numericInput("giantlvl", label = h5("Level"), 
-                                value = 1,
-                                min = 1,
-                                max = 6),  
-                   br(),  
-                   br(), 
-                   numericInput("wblvl", label = h5("Level"), 
-                                value = 1,
-                                min = 1,
-                                max = 6)
-                   )
+            column(3, 
+                  
+                   sliderInput("num_barb", label = h5("Barbarians"),
+                               min = 0, max = 200, value = 40),  
+               
+                   sliderInput("num_arch", label = h5("Archers"),
+                               min = 0, max = 200, value = 60),  
+              
+                   sliderInput("num_gob", label = h5("Goblins"),
+                               min = 0, max = 200, value = 30),  
+              
+                   sliderInput("num_giant", label = h5("Giants"),
+                               min = 0, max = 40, value = 4)
+                   ),
             
+            column(3, 
+                   sliderInput("num_mins", label = h5("Minions"),
+                               min = 0,  max = 100, value = 0)
+                   )
+        
             )   
         )
     )
