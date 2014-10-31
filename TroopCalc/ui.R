13f4f6958a881ef96547482d5c41a87931b12943
shannon buckley <shannon.buckley@ucsf.edu>
2014-10-30 23:12:33 -0700

    v1.3: Significant UI streamline. Just the basics.

--- TroopCalc/ui.R
+++ TroopCalc/ui.R
@@ -2,7 +2,7 @@ library(shiny)
 source("helpers.R")
 # ui.R
 # 
-VERSION = '1.2'
+VERSION = '1.3'
 
 shinyUI(fluidPage(title="TroopCalculator",
 #     titlePanel("TroopCalc"),
@@ -10,146 +10,93 @@ shinyUI(fluidPage(title="TroopCalculator",
     
     tags$head(
         tags$link(rel="stylesheet", type = "text/css", href="bootstrap.css")),
-    headerPanel(h1(paste("Welcome to TroopCalc", VERSION), align = "center")),
+        headerPanel(h1(paste("Welcome to TroopCalc", VERSION), align = "center")),
     #titlePanel("Welcome to TroopCalc"),
-        
-    sidebarLayout(
-    sidebarPanel(
-        h3(helpText("Enter some basic information...")),
-        fluidRow(theme = "bootstrap.css",
-            column(3,
-                   radioButtons("th", label = h5("Town Hall Level"),
-                                choices = list("5" = 1, "6" = 2,
-                                               "7" = 3, "8" = 4,
-                                               "9" = 5, "10" = 6),
-                                selected = 1)),
-            column(3, 
-                   radioButtons("lab", label = h5("Laboratory Level"),
-                                choices = list("1" = 1, "2" = 2,
-                                               "3" = 3, "4" = 4,
-                                               "5" = 5, "6" = 6),
-                                selected = 1)),
-            column(3, 
-                   radioButtons("spellfact", label = h5("Spell Factory Level"),
-                                choices = list("1" = 1, "2" = 2,
-                                               "3" = 3, "4" = 4,
-                                               "5" = 5, "6" = 6),
-                                selected = 1)),
-            column(3, 
-                   checkboxGroupInput("checkGroup", 
-                                      label = h5("Army Camps"), 
-                                      choices = list("Camp 1" = 1, 
-                                                     "Camp 2" = 2, 
-                                                     "Camp 3" = 3,
-                                                     "Camp 4" = 4),
-                                      selected = 1))
-        )
-     ),
-
-    mainPanel(
-        fluidRow(
-            column(3, 
-                   checkboxGroupInput("army_group", 
-                                      label = h4("Troops Available", align = "center"), 
-                                      choices = list("Barbarians" = 1, 
-                                                     "Archers" = 2, 
-                                                     "Goblins" = 3,
-                                                     "Giants" = 4,
-                                                     "Wallbreakers" = 5,
-                                                     "Wizards" = 6,
-                                                     "Healers" = 7,
-                                                     "Dragons" = 8,
-                                                     "Pekkas" = 9),
-                                      selected = 1)),
-            column(4,
-                   radioButtons("barb_lvl", label = h4("Barbarians", align = "center", color = "black"),
-                                choices = list("1" = 1, "2" = 2,
-                                               "3" = 3, "4" = 4,
-                                               "5" = 5, "6" = 6),
-                                selected = 4, inline=T),
-                   
-                   radioButtons("arch_lvl", label = h4("Archers", align = "center"),
-                                choices = list("1" = 1, "2" = 2,
-                                               "3" = 3, "4" = 4,
-                                               "5" = 5, "6" = 6),
-                                selected = 4, inline=T),
-                   
-                   radioButtons("gob_lvl", label = h4("Goblins", align = "center"),
-                                choices = list("1" = 1, "2" = 2,
-                                               "3" = 3, "4" = 4,
-                                               "5" = 5, "6" = 6),
-                                selected = 4, inline=T),
-                   
-                   radioButtons("giant_lvl", label = h4("Giants", align = "center"),
-                                choices = list("1" = 1, "2" = 2,
-                                               "3" = 3, "4" = 4,
-                                               "5" = 5, "6" = 6),
-                                selected = 4, inline=T),
-                   
-                   radioButtons("wb_lvl", label = h4("Breakers", align = "center"),
-                                choices = list("1" = 1, "2" = 2,
-                                               "3" = 3, "4" = 4,
-                                               "5" = 5, "6" = 6),
-                                selected = 4, inline=T),
-                   
-                   radioButtons("drag_lvl", label = h4("Dragons", align = "center"),
-                                choices = list("1" = 1, "2" = 2,
-                                               "3" = 3, "4" = 4),
-                                selected = 2, inline=T)
-                   ),
+        sidebarLayout(position="right",
+            sidebarPanel(
+                                radioButtons("barb_lvl", label = h4("Barbarians", align = "center", color = "black"),
+                                             choices = list("1" = 1, "2" = 2,
+                                                            "3" = 3, "4" = 4,
+                                                            "5" = 5, "6" = 6),
+                                             selected = 4, inline=T),
+                                
+                                radioButtons("arch_lvl", label = h4("Archers", align = "center"),
+                                             choices = list("1" = 1, "2" = 2,
+                                                            "3" = 3, "4" = 4,
+                                                            "5" = 5, "6" = 6),
+                                             selected = 4, inline=T),
+                                
+                                radioButtons("gob_lvl", label = h4("Goblins", align = "center"),
+                                             choices = list("1" = 1, "2" = 2,
+                                                            "3" = 3, "4" = 4,
+                                                            "5" = 5, "6" = 6),
+                                             selected = 4, inline=T),
+                                
+                                radioButtons("giant_lvl", label = h4("Giants", align = "center"),
+                                             choices = list("1" = 1, "2" = 2,
+                                                            "3" = 3, "4" = 4,
+                                                            "5" = 5, "6" = 6),
+                                             selected = 4, inline=T),
+                                
+                                radioButtons("wb_lvl", label = h4("Breakers", align = "center"),
+                                             choices = list("1" = 1, "2" = 2,
+                                                            "3" = 3, "4" = 4,
+                                                            "5" = 5, "6" = 6),
+                                             selected = 4, inline=T),
+                                
+                                radioButtons("drag_lvl", label = h4("Dragons", align = "center"),
+                                             choices = list("1" = 1, "2" = 2,
+                                                            "3" = 3, "4" = 4),
+                                             selected = 2, inline=T)
+                         
+                     ),
+        mainPanel(
+            h3(helpText("Compose an army", align = "center")),
             fluidRow(
-                h4("Troop Costs", align= "center"),
-                h5(textOutput("barbcost")),
-                h5(textOutput("archcost")),
-                h5(textOutput("gobcost")),
-                h5(textOutput("giantcost")),
-                h5(textOutput("wbcost")),
-                h5(textOutput("dragcost"))
+                
+                column(4, 
+                       
+                       
+                       sliderInput("num_barb", label = h5("Barbarians"),
+                                   min = 0, max = 200, value = 44),  
+                       
+                       sliderInput("num_arch", label = h5("Archers"),
+                                   min = 0, max = 200, value = 60),  
+                       
+                       sliderInput("num_gob", label = h5("Goblins"),
+                                   min = 0, max = 200, value = 40),  
+                       
+                       sliderInput("num_giant", label = h5("Giants"),
+                                   min = 0, max = 40, value = 8)
+                ),
+                
+                column(3, 
+                       sliderInput("num_wb", label = h5("Wall Breakers"),
+                                   min = 0, max = 14, value = 8, step = 1),
+                       sliderInput("num_minion", label = h5("Minions"),
+                                   min = 0,  max = 100, value = 0),
+                       sliderInput("num_drag", label = h5("Dragons"),
+                                   min = 0, max = 10, value = 0)
+                )
             ),
             
-            fluidRow(
-                h4("Total Army Costs", align= "left"),
-                h5(textOutput("totalcost")),
-                h5(textOutput("totalsize"))
-            )
-        ),
-        br(),
-        hr(),
-        h4("Compose an army", align = "center"),
-        fluidRow(
-            
-            column(3,
-                   numericInput("army_size", 
-                                label = h5("Total army size"), 
-                                value = 200)
-                   
-                   ),
-            
-            column(3, 
-                  
-                   sliderInput("num_barb", label = h5("Barbarians"),
-                               min = 0, max = 200, value = 44),  
-               
-                   sliderInput("num_arch", label = h5("Archers"),
-                               min = 0, max = 200, value = 60),  
-              
-                   sliderInput("num_gob", label = h5("Goblins"),
-                               min = 0, max = 200, value = 40),  
-              
-                   sliderInput("num_giant", label = h5("Giants"),
-                               min = 0, max = 40, value = 8)
-                   ),
-            
-            column(3, 
-                   sliderInput("num_wb", label = h5("Wall Breakers"),
-                               min = 0, max = 14, value = 8, step = 1),
-                   sliderInput("num_minion", label = h5("Minions"),
-                               min = 0,  max = 100, value = 0),
-                   sliderInput("num_drag", label = h5("Dragons"),
-                               min = 0, max = 10, value = 0)
-                   )
-        
+        fluidRow(        
+            column(5,
+                   h3(helpText("Troop Costs", align= "center")),
+                   h5(textOutput("barbcost")),
+                   h5(textOutput("archcost")),
+                   h5(textOutput("gobcost")),
+                   h5(textOutput("giantcost")),
+                   h5(textOutput("wbcost")),
+                   h5(textOutput("dragcost"))
+                ),
+            column(5, 
+                    h3(helpText("Army Totals", align= "left")),
+                    h5(textOutput("totalcost")),
+                    h5(textOutput("totalsize"))
+                )
             )
         )
     )
-))
+)
+)
